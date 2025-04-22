aad30d12edf39e141a177a90500197dd7a55a08c
Scarlet1107 <shougo.ushiro@gmail.com>
2025-04-22 14:18:04 +0900

    add: protect sign-in and sign-up page from logged in user.

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -1,8 +1,17 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // Check if the user is already logged in
+  const supabase = await createClient();
+  const user = await supabase.auth.getUser();
+  if (user) {
+    redirect("/protected/home");
+  }
   return (
     <div className="max-w-7xl flex flex-col gap-12 items-start">{children}</div>
   );
