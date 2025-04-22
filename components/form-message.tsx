af0262a33d0f524c10f66f2319afb5ac7d499ea7
Scarlet1107 <shougo.ushiro@gmail.com>
2025-04-22 14:18:21 +0900

    fix: update error message

--- components/form-message.tsx
+++ components/form-message.tsx
@@ -13,9 +13,7 @@ export function FormMessage({ message }: { message: Message }) {
       )}
       {"error" in message && (
         <div className="text-foreground border-l-2 border-destructive-foreground px-4">
-          <h2 className="text-xl">
-            予期せぬエラーが発生しました。再度アカウントを作成してください。
-          </h2>
+          <h2 className="text-xl">予期せぬエラーが発生しました</h2>
           <span className="text-red-500">エラー: {message.error}</span>
         </div>
       )}
