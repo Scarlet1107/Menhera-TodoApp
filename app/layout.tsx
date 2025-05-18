cd2bf3bc479f183213eabda3ea7bcc7cb291e9e2
Scarlet1107 <shougo.ushiro@gmail.com>
2025-05-18 19:25:46 +0900

    fix: pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import HeaderAuth from "@/components/header-auth";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
-import Link from "next/link";
 import "./globals.css";
 import { createClient } from "@/utils/supabase/server";
 import { MobileNavigation } from "@/components/mobileNavigation";
@@ -23,7 +22,7 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
-  title: "メンヘラTodoアプリ",
+  title: "メンヘラTodo",
   description:
     "めんどくさくて続かないTodoアプリ、ヘラちゃんと一緒にがんばりませんか?",
   openGraph: {
@@ -72,6 +71,17 @@ export default async function RootLayout({
 
   return (
     <html lang="ja" className={geistSans.className} suppressHydrationWarning>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/pwa/192x192.png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/pwa/512x512.png" sizes="512x512" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
+
+      </head>
       <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
