935aa0ee0c0c36a3649812c5cf5533a83766d393
Scarlet1107 <shougo.ushiro@gmail.com>
2025-10-26 14:56:59 +0900

    add: robots.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,6 @@ export default async function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
       </head>
